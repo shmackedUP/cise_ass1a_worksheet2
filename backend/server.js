8b1d9a834b093c182b056d0826a769bd992b367b
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-07-29 16:56:09 +1200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const articles = require("./dummydata/articles");
+
+const server = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+
+server.get('/', (req,res) => {
+    res.send("API is running")
+})
+
+server.get('/api/articles', (req,res) => {
+    res.json(articles);
+});
+
+
+
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
